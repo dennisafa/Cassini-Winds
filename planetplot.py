c69d6e89ffc8a09c84b57f4573a1be6eb2f41e00
Dennis Afanasev <dennis.afa@gmail.com>
2018-06-27 16:01:57 -0400

    update

--- planetplot.py
+++ planetplot.py
@@ -2,8 +2,26 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 file = np.genfromtxt("cb2_winds.txt", dtype=float, usecols=(0,1))
+file2 = np.genfromtxt("cb2_winds.txt", dtype=float, usecols=(0,2))
 print(file)
-file.reshape(2,2)
+y = file[:,0]
+x = file[:,1]
+print(file2)
 
-pl.plot(file[0:], )
+y2 = file[:,0]
+x2 = file[:,1]
+
+figplot = pl.figure(figsize=(6,3))
+
+axes1 = figplot.add_subplot(1,2,1)
+axes2 = figplot.add_subplot(1,2,2)
+
+axes1.set_ylabel('u (m/s)')
+axes1.set_xlabel('lat (pc)')
+axes1.plot(y,x, '--b')
+
+axes2.set_ylabel('lat (pc)')
+axes2.set_xlabel('u (m/s)')
+axes2.plot(y2,x2, '--g')
+
 pl.show()
